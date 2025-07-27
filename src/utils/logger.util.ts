af695bf13c0ba30eb01151ddb62ebee8a7d9cb77
Hung Nguyen <nguyenmanhhung071203@gmail.com>
2025-07-27 20:11:05 +0700

    update

--- src/utils/logger.util.ts
+++ src/utils/logger.util.ts
@@ -9,10 +9,10 @@ const levelColors: Record<LogLevel, chalk.Chalk> = {
   debug: chalk.greenBright,
 };
 
-function log(level: LogLevel, message: string) {
+function log(level: LogLevel, ...args: any[]) {
   const time = chalk.gray(new Date().toLocaleTimeString("en-US", { hour12: false }));
   const levelLabel = levelColors[level](level.toUpperCase().padEnd(5));
-  console.log(`${levelLabel} - ${time} - ${message}`);
+  console.log(`${levelLabel} - ${time} -`, ...args);
 }
 
 export const Logger = {
